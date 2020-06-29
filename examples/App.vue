fb0496befaf59a14ee872c188309786191bb3731
Lsnsh <zehexin6@gmail.com>
2020-06-29 19:31:05 +0800

    update: (examples) home page style optimization and layout adjustment

--- examples/App.vue
+++ examples/App.vue
@@ -1,154 +1,196 @@
 <template>
-  <div class="p-30">
-    <!-- 表单 -->
-    <div class="fs-22 color-blue text-center">表单</div>
-    <hr />
-    <!-- Button -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Button</span><span class="mlr-20">-</span
-        ><span class="fs-18">按钮</span>
+  <div class="page">
+    <div class="page-header">
+      <div style="font-size: 32px;">WeUI-Vue</div>
+      <div style="color: rgba(0, 0, 0, 0.5);">
+        WeUI-Vue 是基于 WeUI 封装的第三方 Vue 组件库，WeUI
+        是一套同微信原生视觉体验一致的基础样式库，由微信官方设计团队为微信内网页和微信小程序量身设计，令用户的使用感知更加统一。
       </div>
-      <hr class="mtb-20" />
-      <wv-button>默认按钮</wv-button>
-      <wv-button loading="loading">默认按钮</wv-button>
-      <wv-button disabled="disabled">默认按钮</wv-button>
-      <wv-button type="primary">主要按钮</wv-button>
-      <wv-button type="warn">危险按钮</wv-button>
-      <div>朴素按钮</div>
-      <wv-button plain="plain">默认按钮</wv-button>
-      <wv-button type="primary" plain="plain">主要按钮</wv-button>
-      <div>按钮大小</div>
-      <wv-button>普通尺寸按钮</wv-button>
-      <wv-button size="mini">小尺寸按钮</wv-button>
-      <wv-button size="mini" loading="loading">小尺寸按钮</wv-button>
-      <wv-button size="mini" disabled="disabled">小尺寸按钮</wv-button>
-      <wv-button type="primary" size="mini">小尺寸按钮</wv-button>
-      <wv-button type="warn" size="mini">小尺寸按钮</wv-button>
-      <wv-button style="width: 100%;">大尺寸按钮</wv-button>
     </div>
-    <!-- Switch -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Switch</span><span class="mlr-20">-</span
-        ><span class="fs-18">开关</span>
-      </div>
-      <hr class="mtb-20" />
-      <wv-switch v-model="isSwitch1">开关</wv-switch>
-    </div>
-    <!-- Checkbox -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Checkbox</span><span class="mlr-20">-</span
-        ><span class="fs-18">复选框</span>
-      </div>
-      <hr class="mtb-20" />
-      <wv-checkbox v-model="isCheckbox1">复选框</wv-checkbox>
-    </div>
-    <!-- 基础组件 -->
-    <div class="fs-22 color-blue text-center">基础组件</div>
-    <hr />
-    <!-- Icon -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Icon</span><span class="mlr-20">-</span
-        ><span class="fs-18">图标</span>
-      </div>
-      <hr class="mtb-20" />
-      <div
-        class="inbl vtal-top w-170 h-170 mb-20 text-center"
-        v-for="sIcon in iconList"
-        :key="sIcon"
-      >
-        <div class="h-80 lh-80">
-          <i :class="sIcon" style="font-size: 0.5rem;"></i>
+    <div class="page-main">
+      <!-- 表单 -->
+      <div class="section">
+        <div class="section-header">表单</div>
+        <div class="section-main">
+          <!-- Button -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Button - 按钮</span>
+            </div>
+            <div class="section-item-main">
+              <wv-button>默认按钮</wv-button>
+              <wv-button loading="loading">默认按钮</wv-button>
+              <wv-button disabled="disabled">默认按钮</wv-button>
+              <wv-button type="primary">主要按钮</wv-button>
+              <wv-button type="warn">危险按钮</wv-button>
+              <div>朴素按钮</div>
+              <wv-button plain="plain">默认按钮</wv-button>
+              <wv-button type="primary" plain="plain">主要按钮</wv-button>
+              <div>按钮大小</div>
+              <wv-button>普通尺寸按钮</wv-button>
+              <wv-button size="mini">小尺寸按钮</wv-button>
+              <wv-button size="mini" loading="loading">小尺寸按钮</wv-button>
+              <wv-button size="mini" disabled="disabled">小尺寸按钮</wv-button>
+              <wv-button type="primary" size="mini">小尺寸按钮</wv-button>
+              <wv-button type="warn" size="mini">小尺寸按钮</wv-button>
+              <wv-button style="width: 100%;">大尺寸按钮</wv-button>
+            </div>
+          </div>
+          <!-- Input -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Input - 输入框</span>
+            </div>
+            <div class="section-item-main">
+              <div style="background: #efef;">
+                <wv-input
+                  v-model="sInputText1"
+                  label="微信号"
+                  placeholder="请输入微信号"
+                ></wv-input>
+                <wv-input
+                  v-model="sInputText2"
+                  label="昵称"
+                  placeholder="请输入昵称"
+                ></wv-input>
+              </div>
+            </div>
+          </div>
+          <!-- Switch -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Switch - 开关</span>
+            </div>
+            <div class="section-item-main">
+              <wv-switch v-model="isSwitch1">开关</wv-switch>
+            </div>
+          </div>
+          <!-- Checkbox -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Checkbox - 复选框</span>
+            </div>
+            <div class="section-item-main">
+              <wv-checkbox v-model="isCheckbox1">复选框</wv-checkbox>
+            </div>
+          </div>
         </div>
-        <div class="h-90 vh-parent">
-          <div class="v w-p100 fs-24 fw-300 color-grey">{{ sIcon }}</div>
+      </div>
+      <!-- 基础组件 -->
+      <div class="section">
+        <div class="section-header">基础组件</div>
+        <div class="section-main">
+          <!-- Icon -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Icon - 图标</span>
+            </div>
+            <div class="section-item-main">
+              <div
+                v-for="sIcon in iconList"
+                :key="sIcon"
+                style="text-align: center;"
+              >
+                <div class="h-80 lh-80">
+                  <i :class="sIcon" style="font-size: 0.5rem;"></i>
+                </div>
+                <div class="h-90 vh-parent">
+                  <div class="v w-p100 fs-24 color-grey">
+                    {{ sIcon }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- LoadMore -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>LoadMore - 加载更多</span>
+            </div>
+            <div class="section-item-main">
+              <wv-load-more tips="正在加载"></wv-load-more>
+              <wv-load-more tips="暂无数据" :loading="false"></wv-load-more>
+              <wv-load-more :loading="false"></wv-load-more>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <!-- LoadMore -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">LoadMore</span><span class="mlr-20">-</span
-        ><span class="fs-18">加载更多</span>
+      <!-- 操作反馈 -->
+      <div class="section">
+        <div class="section-header">操作反馈</div>
+        <div class="section-main">
+          <!-- Dialog -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Dialog - 对话框</span>
+            </div>
+            <div class="section-item-main">
+              <wv-button
+                @click="
+                  clickShowDialog({
+                    title: '弹窗标题',
+                    content:
+                      '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+                    distinguishCancelAndClose: true
+                  })
+                "
+                >iOS-有标题Dialog</wv-button
+              >
+              <wv-button
+                @click="
+                  clickShowDialog({
+                    content:
+                      '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+                    showCancelButton: false,
+                    closeOnClickMask: false
+                  })
+                "
+                >iOS-无标题Dialog</wv-button
+              >
+              <wv-button
+                @click="
+                  clickShowDialog({
+                    title: '弹窗标题',
+                    content:
+                      '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+                    android: true
+                  })
+                "
+                >Android-无标题Dialog</wv-button
+              >
+              <wv-button
+                @click="
+                  clickShowDialog({
+                    content:
+                      '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
+                    android: true
+                  })
+                "
+                >Android-无标题Dialog</wv-button
+              >
+            </div>
+          </div>
+          <!-- Toast -->
+          <div class="section-item">
+            <div class="section-item-header">
+              <span>Toast - 弹出式提示</span>
+            </div>
+            <div class="section-item-main">
+              <wv-button @click="clickShowToast({ text: '提示信息' })"
+                >纯文本提示</wv-button
+              >
+              <wv-button
+                @click="clickShowToast({ text: '已完成', type: 'success' })"
+                >成功提示</wv-button
+              >
+              <wv-button
+                @click="clickShowToast({ text: '加载中', type: 'loading' })"
+                >加载中提示</wv-button
+              >
+            </div>
+          </div>
+        </div>
       </div>
-      <hr class="mtb-20" />
-      <wv-load-more tips="正在加载"></wv-load-more>
-      <wv-load-more tips="暂无数据" :loading="false"></wv-load-more>
-      <wv-load-more :loading="false"></wv-load-more>
-    </div>
-    <!-- 操作反馈 -->
-    <div class="fs-22 color-blue text-center">操作反馈</div>
-    <hr />
-    <!-- Dialog -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Dialog</span><span class="mlr-20">-</span
-        ><span class="fs-18">对话框</span>
-      </div>
-      <hr class="mtb-20" />
-      <wv-button
-        @click="
-          clickShowDialog({
-            title: '弹窗标题',
-            content:
-              '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
-            distinguishCancelAndClose: true
-          })
-        "
-        >iOS-有标题Dialog</wv-button
-      >
-      <wv-button
-        @click="
-          clickShowDialog({
-            content:
-              '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
-            showCancelButton: false,
-            closeOnClickMask: false
-          })
-        "
-        >iOS-无标题Dialog</wv-button
-      >
-      <wv-button
-        @click="
-          clickShowDialog({
-            title: '弹窗标题',
-            content:
-              '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
-            android: true
-          })
-        "
-        >Android-无标题Dialog</wv-button
-      >
-      <wv-button
-        @click="
-          clickShowDialog({
-            content:
-              '弹窗内容，告知当前状态、信息和解决方法，描述文字尽量控制在三行内',
-            android: true
-          })
-        "
-        >Android-无标题Dialog</wv-button
-      >
-    </div>
-    <!-- Toast -->
-    <div class="mtb-20">
-      <div class="fw-400">
-        <span class="fs-18">Toast</span><span class="mlr-20">-</span
-        ><span class="fs-18">弹出式提示</span>
-      </div>
-      <hr class="mtb-20" />
-      <wv-button @click="clickShowToast({ text: '提示信息' })"
-        >纯文本提示</wv-button
-      >
-      <wv-button @click="clickShowToast({ text: '已完成', type: 'success' })"
-        >成功提示</wv-button
-      >
-      <wv-button @click="clickShowToast({ text: '加载中', type: 'loading' })"
-        >加载中提示</wv-button
-      >
     </div>
   </div>
 </template>
@@ -159,6 +201,8 @@ export default {
     return {
       isSwitch1: false,
       isCheckbox1: true,
+      sInputText1: "",
+      sInputText2: "",
       iconList: [
         "weui-icon-circle",
         "weui-icon-download",
@@ -198,34 +242,37 @@ export default {
 };
 </script>
 
+<style lang="less">
+@import "./main.less";
+</style>
+
 <style scoped>
-.mtb-20 {
-  margin-top: 20px;
-  margin-bottom: 20px;
-}
-.mlr-20 {
-  margin-left: 20px;
-  margin-right: 20px;
-}
-.p-30 {
+/* .page {} */
+.page-header {
   padding: 30px;
 }
-.pl-30 {
-  padding-left: 30px;
+.page-main {
+  text-align: center;
 }
-.fs-22 {
+.section-header {
+  padding: 10px 0;
+  text-align: center;
   font-size: 22px;
-}
-.fs-18 {
-  font-size: 18px;
-}
-.fw-400 {
-  font-weight: 400;
-}
-.color-blue {
   color: #409eff;
 }
-.text-center {
+/* .section-main {} */
+.section-item {
+  margin-bottom: 20px;
+}
+.section-item-header {
+  padding: 10px;
+  border-radius: 2px;
   text-align: center;
+  font-size: 18px;
+  color: #6f6f6f;
+  background: #ffffff;
+}
+.section-item-main {
+  padding: 20px 0;
 }
 </style>
